73af27981a1ddf229253eb598f1c7e37931b7c02
Collins Lagat <cklagat@gmail.com>
2022-01-14 11:20:24 +0300

    feat: checkout decoration not needed

--- app/controllers/spree/checkout_controller_decorator.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-    module CheckoutControllerDecorator
-        def xxx
-            "XXXX"
-        end
-    end
-end
-
-::Spree::CheckoutController.prepend(Spree::CheckoutControllerDecorator)
