0811df36986ef316f00cecf51b4da9162702bfa9
Collins Lagat <cklagat@gmail.com>
2022-01-14 22:37:30 +0300

    wip

--- app/models/spree_flutterwave/gateway/flutterwave.rb
+++ app/models/spree_flutterwave/gateway/flutterwave.rb
@@ -14,15 +14,22 @@ module SpreeFlutterwave
       end
 
       def source_required?
-        false
+        true
       end
 
       def provider
-        ::Flutterwave.new(preferred_public_key, preferred_secret_key, preferred_encryption_key)
+        ::Flutterwave.new(preferred_public_key, preferred_secret_key, preferred_encryption_key, 'https://ravesandboxapi.flutterwave.com')
       end
 
       def provider_class
         ::Flutterwave
+      end
+
+      def authorize(_money_in_cents, _source, _gateway_options)
+        tx = Transactions.new(provider)
+        # tx.verify_transaction(source.transaction_id)
+        ActiveMerchant::Billing::Response.new(true, 'Flutterwave Gateway: Forced Success', { message: 'Flutterwave Gateway: Forced success' },
+                                              test: true)
       end
     end
   end
