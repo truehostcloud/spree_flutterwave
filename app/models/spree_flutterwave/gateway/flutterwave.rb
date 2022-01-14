bf36d03f5fbce0eca22b9f6e1d1be40323417249
Collins Lagat <cklagat@gmail.com>
2022-01-14 11:22:29 +0300

    feat: flutterwave gateway wip

--- app/models/spree_flutterwave/gateway/flutterwave.rb
+++ app/models/spree_flutterwave/gateway/flutterwave.rb
@@ -1,8 +1,28 @@
 module SpreeFlutterwave
   module Gateway
     class Flutterwave < Spree::Gateway
+      preference :public_key, :string
+      preference :secret_key, :string
+      preference :encryption_key, :string
+
       def method_type
         'flutterwave'
+      end
+
+      def payment_source_class
+        Spree::FlutterwaveCheckout
+      end
+
+      def source_required?
+        false
+      end
+
+      def provider
+        ::Flutterwave.new(preferred_public_key, preferred_secret_key, preferred_encryption_key)
+      end
+
+      def provider_class
+        ::Flutterwave
       end
     end
   end
