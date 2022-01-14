ceab9b47b56b55f7f6dbe21cefee4e824540052f
Collins Lagat <cklagat@gmail.com>
2022-01-14 17:02:12 +0300

    feat: add table name and validation

--- app/models/spree_flutterwave/flutterwave_checkout.rb
+++ app/models/spree_flutterwave/flutterwave_checkout.rb
@@ -1,7 +1,13 @@
 module SpreeFlutterwave
   class FlutterwaveCheckout < ApplicationRecord
+    self.table_name = 'spree_flutterwave_checkouts'
+
     has_one :payment, as: :source, class_name: 'Spree::Payment', dependent: :destroy
     has_one :order, through: :payment, dependent: :destroy
-    belongs_to :payment_method
+    belongs_to :payment_method, class_name: 'Spree::PaymentMethod'
+
+    validates :transaction_id, presence: true
+    validates :transaction_ref, presence: true
+    validates :status, presence: true
   end
 end
