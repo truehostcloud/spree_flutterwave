c5d9d52e9b0809fa75ab72dd426dd4d5dfd98b81
Collins Lagat <cklagat@gmail.com>
2022-01-14 11:19:42 +0300

    feat: update migration

--- db/migrate/20220105115118_create_spree_flutterwave_gateway_flutterwaves.rb
+++ db/migrate/20220105115118_create_spree_flutterwave_gateway_flutterwaves.rb
@@ -1,8 +1,11 @@
 class CreateSpreeFlutterwaveGatewayFlutterwaves < ActiveRecord::Migration[6.1]
   def change
-    create_table :spree_flutterwave_checkouts do |t|
-      t.string :transaction_id, index: true
+    create_table :spree_flutterwave_checkouts, if_not_exists: true do |t|
+      t.bigint :transaction_id, index: true
       t.string :transaction_ref, index: true
+      t.bigint :payment_method_id, index: true
+      t.bigint :user_id, index: true
+      t.string :status
 
       t.timestamps
     end
