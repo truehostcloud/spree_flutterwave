72f9258f9588f95ff139d4d4c0353088c0574d10
Collins Lagat <cklagat@gmail.com>
2022-01-18 10:34:20 +0300

    fix: handle wheb user isn't logged in

--- app/models/spree_flutterwave/checkout_controller_decorator.rb
+++ app/models/spree_flutterwave/checkout_controller_decorator.rb
@@ -5,6 +5,8 @@ module SpreeFlutterwave
     end
 
     def load_flutterave_payment_sources
+      return unless try_spree_current_user.respond_to?(:flutterwave_checkout)
+
       @flutterwave_payment_sources = try_spree_current_user.flutterwave_checkout(@order)
     end
   end
