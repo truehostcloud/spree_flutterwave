05e5530558eee1284a80e1abf14c98720314822b
Collins Lagat <cklagat@gmail.com>
2022-01-20 13:35:46 +0300

    feat: load flutterwave payment method directly

--- app/models/spree_flutterwave/checkout_controller_decorator.rb
+++ app/models/spree_flutterwave/checkout_controller_decorator.rb
@@ -1,11 +1,11 @@
 module SpreeFlutterwave
   module CheckoutControllerDecorator
     def self.prepended(base)
-      base.before_action :load_flutterave_payment_source
+      base.before_action :load_flutterave_payment_method
     end
 
-    def load_flutterave_payment_source
-      @flutterwave_payment_source = SpreeFlutterwave::FlutterwaveCheckout.where(transaction_ref: @order.number).last
+    def load_flutterave_payment_method
+      @flutterwave_payment_method = Spree::PaymentMethod.find_by(type: 'SpreeFlutterwave::Gateway::Flutterwave')
     end
   end
 end
