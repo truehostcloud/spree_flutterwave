1e3d69204724a6b398fa07f77459a9cffb74f55c
Collins Lagat <cklagat@gmail.com>
2022-01-20 14:37:20 +0300

    feat: add helper method to check whether flutterwave is selected

--- app/models/spree_flutterwave/checkout_controller_decorator.rb
+++ app/models/spree_flutterwave/checkout_controller_decorator.rb
@@ -2,10 +2,15 @@ module SpreeFlutterwave
   module CheckoutControllerDecorator
     def self.prepended(base)
       base.before_action :load_flutterave_payment_method
+      base.helper_method :flutterwave_chosen?
     end
 
     def load_flutterave_payment_method
       @flutterwave_payment_method = Spree::PaymentMethod.find_by(type: 'SpreeFlutterwave::Gateway::Flutterwave')
+    end
+
+    def flutterwave_chosen?
+      @order.valid_payment.payment_method.type == 'SpreeFlutterwave::Gateway::Flutterwave'
     end
   end
 end
