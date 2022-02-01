8b3d643f8271b0eb2282f0e26e02875cebad696d
Collins Lagat <cklagat@gmail.com>
2022-02-01 11:56:11 +0300

    Revert "feat: make migration reversible"
    This reverts commit 8361321b504877fdd851ab3bde0839994eaaa882.

--- db/migrate/20220128130006_fix_raw_response_data_type.rb
+++ db/migrate/20220128130006_fix_raw_response_data_type.rb
@@ -1,15 +1,5 @@
 class FixRawResponseDataType < ActiveRecord::Migration[6.1]
   def change
-    reversible do |dir|
-      change_table :spree_flutterwave_checkouts do |t|
-        dir.up do
-          t.json :raw_response, :json
-        end
-
-        dir.down do
-          t.jsonb :raw_response
-        end
-      end
-    end
+    change_column :spree_flutterwave_checkouts, :raw_response, :json
   end
 end
