b5fa8bf053eb9d4d9bd147a64def1b1d8f850f6d
Collins Lagat <cklagat@gmail.com>
2022-02-03 11:44:19 +0300

    fix: add module

--- app/controllers/spree/spree_flutterwave/checkout_controller_decorator.rb
+++ app/controllers/spree/spree_flutterwave/checkout_controller_decorator.rb
@@ -1,20 +1,22 @@
-module SpreeFlutterwave
-  module CheckoutControllerDecorator
-    def self.prepended(base)
-      base.before_action :load_flutterave_payment_method
-      base.helper_method :flutterwave_chosen?
-    end
+module Spree
+  module SpreeFlutterwave
+    module CheckoutControllerDecorator
+      def self.prepended(base)
+        base.before_action :load_flutterave_payment_method
+        base.helper_method :flutterwave_chosen?
+      end
 
-    def load_flutterave_payment_method
-      @flutterwave_payment_method = Spree::PaymentMethod.find_by(type: 'SpreeFlutterwave::Gateway::Flutterwave')
-    end
+      def load_flutterave_payment_method
+        @flutterwave_payment_method = Spree::PaymentMethod.find_by(type: 'SpreeFlutterwave::Gateway::Flutterwave')
+      end
 
-    def flutterwave_chosen?
-      return @order.valid_payment.payment_method.type == 'SpreeFlutterwave::Gateway::Flutterwave' if @order.valid_payment.present?
+      def flutterwave_chosen?
+        return @order.valid_payment.payment_method.type == 'SpreeFlutterwave::Gateway::Flutterwave' if @order.valid_payment.present?
 
-      false
+        false
+      end
     end
   end
 end
 
-::Spree::CheckoutController.prepend(SpreeFlutterwave::CheckoutControllerDecorator)
+::Spree::CheckoutController.prepend(::Spree::SpreeFlutterwave::CheckoutControllerDecorator)
