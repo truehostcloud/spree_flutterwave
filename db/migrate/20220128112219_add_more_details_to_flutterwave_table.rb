44120899e46a2d94e40dce33da6d89ed27d3eb14
Collins Lagat <cklagat@gmail.com>
2022-04-14 09:33:59 +0300

    `if_not_exists` migrations

--- db/migrate/20220128112219_add_more_details_to_flutterwave_table.rb
+++ db/migrate/20220128112219_add_more_details_to_flutterwave_table.rb
@@ -1,16 +1,16 @@
 class AddMoreDetailsToFlutterwaveTable < ActiveRecord::Migration[6.1]
   def change
-    add_column :spree_flutterwave_checkouts, :currency, :string
+    add_column :spree_flutterwave_checkouts, :currency, :string, if_not_exists: true
 
-    add_column :spree_flutterwave_checkouts, :amount, :decimal, precision: 10, scale: 2, default: '0.0', null: false
-    add_column :spree_flutterwave_checkouts, :charged_amount, :decimal, precision: 10, scale: 2, default: '0.0', null: false
-    add_column :spree_flutterwave_checkouts, :app_fee, :decimal, precision: 10, scale: 2, default: '0.0', null: false
-    add_column :spree_flutterwave_checkouts, :merchant_fee, :decimal, precision: 10, scale: 2, default: '0.0', null: false
-    add_column :spree_flutterwave_checkouts, :amount_settled, :decimal, precision: 10, scale: 2, default: '0.0', null: false
+    add_column :spree_flutterwave_checkouts, :amount, :decimal, precision: 10, scale: 2, default: '0.0', null: false, if_not_exists: true
+    add_column :spree_flutterwave_checkouts, :charged_amount, :decimal, precision: 10, scale: 2, default: '0.0', null: false, if_not_exists: true
+    add_column :spree_flutterwave_checkouts, :app_fee, :decimal, precision: 10, scale: 2, default: '0.0', null: false, if_not_exists: true
+    add_column :spree_flutterwave_checkouts, :merchant_fee, :decimal, precision: 10, scale: 2, default: '0.0', null: false, if_not_exists: true
+    add_column :spree_flutterwave_checkouts, :amount_settled, :decimal, precision: 10, scale: 2, default: '0.0', null: false, if_not_exists: true
 
-    add_column :spree_flutterwave_checkouts, :auth_model, :string
-    add_column :spree_flutterwave_checkouts, :narration, :string
+    add_column :spree_flutterwave_checkouts, :auth_model, :string, if_not_exists: true
+    add_column :spree_flutterwave_checkouts, :narration, :string, if_not_exists: true
 
-    add_column :spree_flutterwave_checkouts, :raw_response, :json
+    add_column :spree_flutterwave_checkouts, :raw_response, :json, if_not_exists: true
   end
 end
