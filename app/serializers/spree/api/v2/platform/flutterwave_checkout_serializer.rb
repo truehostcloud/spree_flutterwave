2a4c259e470124e6d731e33c07bd46b273115aa8
Collins Lagat <cklagat@gmail.com>
2023-05-15 13:10:11 +0300

    feat: add timestamps

--- app/serializers/spree/api/v2/platform/flutterwave_checkout_serializer.rb
+++ app/serializers/spree/api/v2/platform/flutterwave_checkout_serializer.rb
@@ -6,7 +6,7 @@ module Spree
           set_type :flutterwave_checkout
 
           attributes :transaction_id, :transaction_ref, :status, :currency, :amount, :charged_amount, :app_fee, :merchant_fee, :amount_settled,
-                     :auth_model, :narration
+                     :auth_model, :narration, :created_at, :updated_at
 
           belongs_to :payment_method
           belongs_to :user
